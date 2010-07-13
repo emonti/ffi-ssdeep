9e780e22bfa4b8e4f30941cabd8970f20be4be5e
Eric Monti <esmonti@gmail.com>
2010-07-13 13:55:35 -0500

    additional fuzzy_hash testcases

--- spec/fuzzy_hash_spec.rb
+++ spec/fuzzy_hash_spec.rb
@@ -1,6 +1,34 @@
 require 'spec_helper'
 
 describe Ssdeep::FuzzyHash do
+  context "basic features" do
+    before(:all) do
+      @fh1 = Ssdeep::FuzzyHash.from_file(sample_file("ssdeep.gemspec"))
+    end
+
+    it "should support a from_string class method to hash from a string buffer" do
+      Ssdeep::FuzzyHash.from_file(sample_file("ssdeep.gemspec")).should be_kind_of(Ssdeep::FuzzyHash)
+    end
+
+    it "should support a from_string class method to hash from a string buffer" do
+      Ssdeep::FuzzyHash.from_string("helu").should be_kind_of(Ssdeep::FuzzyHash)
+    end
+
+    it "should support a compare method to compare one FuzzyHash to another" do
+      fh2 = Ssdeep.hash_file(sample_file("ffi-ssdeep.gemspec"))
+      sh = Ssdeep.hash_string("helu")
+      @fh1.compare(@fh1).should == 100
+      @fh1.compare(fh2).should > 90
+      @fh1.compare(sh).should == 0
+    end
+
+    it "should raise an exception when comparing null hash data" do
+      hh = Ssdeep::FuzzyHash.new()
+      lambda{ @fh1.compare(hh)}.should raise_error(StandardError)
+      lambda{ hh.compare(@fh1)}.should raise_error(StandardError)
+    end
+  end
+
   context "marshal serialization" do
     before(:all) do
       @fh = Ssdeep::FuzzyHash.from_file(sample_file("ssdeep.gemspec"))
